22923a64e63d02c63facb945b7117854f9a2c7d1
Matteo Pacini <ispeakprogramming@gmail.com>
2015-04-08 22:02:21 +0200

    First commit

---
+++ test.s
@@ -0,0 +1,24 @@
+; All possible examples
+
+; ADC (ADd with Carry) 
+ 
+ADC #$44
+ADC $44
+ADC $44,X
+ADC $4400
+ADC $4400,X
+ADC $4400,Y
+ADC ($44,X)
+ADC ($44),Y
+
+;AND (bitwise AND with accumulator)
+
+AND #$44
+AND $44
+AND $44,X
+AND $4400
+AND $4400,X
+AND $4400,Y
+AND ($44,X)
+AND ($44),Y
+
